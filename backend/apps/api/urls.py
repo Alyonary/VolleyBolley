3c1030e9e21bc18668f2ab59ad02957470adb5db
zerin2 <vla-sterlyukin@ya.ru>
2025-08-06 21:08:18 +0300

    ...

--- backend/apps/api/urls.py
+++ backend/apps/api/urls.py
@@ -2,7 +2,7 @@ from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
 
 from apps.courts.views import CourtViewSet
-from apps.event.views import GameViewSet
+# from apps.event.views import GameViewSet
 
 from .views import LogoutView
 from apps.locations.views import CountryListView
@@ -11,7 +11,7 @@ app_name = 'api'
 
 api_v1 = DefaultRouter()
 api_v1.register('courts', CourtViewSet, basename='courts')
-api_v1.register(r'games', GameViewSet, basename='games')
+# api_v1.register(r'games', GameViewSet, basename='games')
 
 urlpatterns = [
     path('', include(api_v1.urls)),
