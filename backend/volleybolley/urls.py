3ef42fe0aa9c3312c06bfdb206b29020590f8a4e
Ilya Shirokov <ilshi2904@gmail.com>
2026-04-15 22:52:48 +0300

    add serializer data for avatar view

--- backend/volleybolley/urls.py
+++ backend/volleybolley/urls.py
@@ -3,9 +3,6 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from django.views.generic import TemplateView
-from rest_framework_simplejwt.views import (
-    TokenObtainPairView,
-)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
